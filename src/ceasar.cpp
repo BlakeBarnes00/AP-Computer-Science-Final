ec9f23460acf93c3bdda433699642894b8e8ffdc
Ellis Barnes <EllisBarnes00@gmail.com>
2017-04-27 22:56:49 -0400

    Finished the pictures and files

--- src/ceasar.cpp
+++ src/ceasar.cpp
@@ -3,17 +3,17 @@
 
 int main(int argc, char **argv)
 {
-  
-  std::string input   = "Hello";
+  std::string input;
+  std::cout << "Enter phrase: ";
+  std::cin >> input;
 
   std::string einput  = encrypt(input, 2);
   std::cout << "Encrypted: " << einput << std::endl;
 
-  std::string dinput = decrypt(einput);
-  std::cout << "Decrypted: " << dinput << std::endl;
-
-  
-
+  // TODO: This is for testing, not working at the moment
+  //std::string dinput = decrypt(einput);
+  //std::cout << "Decrypted: " << dinput << std::endl;
+  //
   std::cin.get();
   return 0;
 }
