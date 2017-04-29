79f5f56cae67828db02c91291cbfb06bd1959604
Ellis Barnes <EllisBarnes00@gmail.com>
2017-04-28 22:01:44 -0400

    Finished with video and documents uploaded as well

--- src/ceasar.cpp
+++ src/ceasar.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <stdio.h>
 #include "ceasar.h"
 
 int main(int argc, char **argv)
 {
   std::string input;
   std::cout << "Enter phrase: ";
-  std::cin >> input;
+  getline(std::cin, input); // Use the getline() to avoid the C++ space character issue
 
   std::string einput  = encrypt(input, 2);
   std::cout << "Encrypted: " << einput << std::endl;
@@ -13,7 +14,7 @@ int main(int argc, char **argv)
   // TODO: This is for testing, not working at the moment
   //std::string dinput = decrypt(einput);
   //std::cout << "Decrypted: " << dinput << std::endl;
-  //
+  
   std::cin.get();
   return 0;
 }
